a06d0e1db2ed7e356db63a2f5a347fbd477e1c4b
AngelNovoa <angelnovoa@angelnovoa.com>
2022-01-17 16:02:57 +0100

    version final con javadoc 53357585T

--- src/angelnovoamartinez53357585t/AngelNovoaMartinez_53357585T.java
+++ src/angelnovoamartinez53357585t/AngelNovoaMartinez_53357585T.java
@@ -5,30 +5,47 @@
 package angelnovoamartinez53357585t;
 
 /**
- *
+ *La clase calcula el salario
  * @author USUARIO
+ * @version final
  */
 
 public class AngelNovoaMartinez_53357585T {
     static final double FACTOR_HORA_EXTRA = 1.2; 
-private String nombreCompleto, dni;
-private double salarioBasePorHora = 10.56;
+    private String nombreCompleto, dni;
+    private double salarioBasePorHora = 10.56;
+
+/**
+ * Datos personales
+ * @param nombreCompleto Nombre del alumno
+ * @param dni Dni del alumno
+ */    
 public AngelNovoaMartinez_53357585T(String nombreCompleto, String dni) {
     this.nombreCompleto = nombreCompleto; 
     this.dni = dni;
 }
+
+/**
+ * Genera salario
+ * @param horasExtra Calcula las horas extra
+ * @return Devuelve el salario final
+ */
 public double getSalarioEsteMes(int horasExtra) {
     double cantidadExtra, salarioFinal; 
     cantidadExtra = horasExtra * this.salarioBasePorHora * FACTOR_HORA_EXTRA; 
     salarioFinal=this.salarioBasePorHora * 40 * 4 + cantidadExtra; 
-    salarioFinal=(double)Math.round(salarioFinal * 100d) / 100d; return salarioFinal;
+    salarioFinal=(double)Math.round(salarioFinal * 100d) / 100d; 
+    return salarioFinal;
 }
-    
+  
+/**
+ * Escribe frase con salario
+ * @param args frase con salario
+ */
       public static void main(String[] args){
     AngelNovoaMartinez_53357585T emp = new AngelNovoaMartinez_53357585T("Angel Novoa","53357585T");
     int horasExtra = 2; 
-    System.out.println("El salario de " + emp.nombreCompleto + " este mes será de " + emp.getSalarioEsteMes(horasExtra) 
-        + " euros, al hacer " + horasExtra + " horas extra.");
+    System.out.println("El salario de " + emp.nombreCompleto + " este mes será de " + emp.getSalarioEsteMes(horasExtra) + " euros, al hacer " + horasExtra + " horas extra.");
       }
 }
     
